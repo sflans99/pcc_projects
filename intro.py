15ca49da72430e630e1164bff73edc9805dd288a
sflans99 <seanflans99@gmail.com>
2025-02-01 22:14:05 -0600

    Python Crash Course project files

---
+++ intro.py
@@ -0,0 +1,23 @@
+
+# message = "Hello World"
+# print(message)
+
+# message = "Hello Newer World"
+# print(message.lower())
+
+# first_name = "Sean"
+# last_name = "Flannery"
+# full_name = f"{first_name} {last_name}"
+# print(f"Hello, {full_name.title()}!")
+
+# strip_test = "python     "
+# print(strip_test)
+
+# print(4+4)
+# print(12-4)
+# print(2*4)
+# print(16/2)
+# fav_num = 13
+# print(f"My favorite number is {fav_num}!")
+
+import this
---
+++ intro_pt2.py
@@ -0,0 +1,48 @@
+import random
+
+# guest_list = ["Steven Gerrard", "Fernando Torres", "Virgil Van Dijk"]
+# for name in guest_list:
+#     print(f"Welcome {name} to the dinner party!")
+
+# digits = []
+
+# for value in range(1, 21):
+#     print(value)
+
+# 4-5 Million count
+
+# milli = list(range(1, 1_000_000))
+# print(sum(milli))
+
+# 4-7 Threes
+# multis = []
+# for value in range(1, 11):
+#     multis.append(value**3)
+#     print(multis[-1])
+
+
+# ---Loops Introduction---
+
+# unconfirmed = ['a', 'b', 'c']
+# confirmed = []
+
+# while unconfirmed:
+#     current_user = unconfirmed.pop()
+#     print(current_user)
+
+#     confirmed.append(current_user)
+
+# ---Input Practice---
+
+# name = input("Username:\n")
+# message = input("Password:\n")
+
+# print(f"Your username is:{name} \nYour password is:{message}")
+
+# ---Multi-line Prompt---
+
+# prompt = "If you tell us who you are, we can personalize the messages you see."
+# prompt += "\nWhat is your first name? "
+# name = input(prompt)
+# print(f"\nHello, {name}!")
+
